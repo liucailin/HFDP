cb3b07d3355115d85044653907ba51c8c367eb7c
liucailin <liu1356@gmail.com>
2014-10-21 00:24:57 +0800

    using double check

--- src/CP5/Singleton.java
+++ src/CP5/Singleton.java
@@ -1,13 +1,23 @@
 package CP5;
 
 public class Singleton {
-	private static Singleton uniqueInstance = new Singleton();
+
+	// the volatile keyword ensures that multiple threads handle the
+	// unique instance
+	private volatile static Singleton uniqueInstance;
 
 	private Singleton() {
 
 	}
 
 	public static synchronized Singleton getInstance() {
+		if (uniqueInstance == null) {
+			synchronized (Singleton.class) {
+				if (uniqueInstance == null) {
+					uniqueInstance = new Singleton();
+				}
+			}
+		}
 		return uniqueInstance;
 	}
 
