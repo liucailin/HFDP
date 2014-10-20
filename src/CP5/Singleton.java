9bb67b5a57b202a52ce15a79e79545489b8db88a
liucailin <liu1356@gmail.com>
2014-10-21 00:20:15 +0800

    change to eagerly singleton.

--- src/CP5/Singleton.java
+++ src/CP5/Singleton.java
@@ -1,16 +1,13 @@
 package CP5;
 
 public class Singleton {
-	private static Singleton uniqueInstance;
+	private static Singleton uniqueInstance = new Singleton();
 
 	private Singleton() {
 
 	}
 
 	public static synchronized Singleton getInstance() {
-		if (uniqueInstance == null) {
-			uniqueInstance = new Singleton();
-		}
 		return uniqueInstance;
 	}
 
