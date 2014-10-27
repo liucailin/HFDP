f0e4b01f89e3e987bd91eb406440d91e01b43f88
liucailin <liu1356@gmail.com>
2014-10-28 00:31:13 +0800

    cp9 two set of menus

---
+++ src/CP9/DinnerMenu.java
@@ -0,0 +1,32 @@
+package CP9;
+
+
+public class DinnerMenu {
+
+	static final int MAX_ITEMS = 6;
+	int numberOfItem;
+	MenuItem[] menuItems;
+
+	public DinnerMenu() {
+		menuItems = new MenuItem[MAX_ITEMS];
+
+		addItem("K&B¡¯s Pancake Breakfast",
+				"Pancakes with scrambled eggs, and toast", true, 2.99);
+		addItem("Regular Pancake Breakfast",
+				"Pancakes with fried eggs, sausage", false, 2.99);
+	}
+
+	public void addItem(String name, String descr, boolean vegetarian,
+			double price) {
+		MenuItem menuItem = new MenuItem(name, descr, vegetarian, price);
+		if (numberOfItem >= MAX_ITEMS)
+			System.err.println("menu is full");
+		menuItems[numberOfItem] = menuItem;
+		numberOfItem++;
+	}
+
+	public MenuItem[] getMenuItems() {
+		return menuItems;
+	}
+
+}
---
+++ src/CP9/PancakeHouseMenu.java
@@ -0,0 +1,28 @@
+package CP9;
+
+import java.util.ArrayList;
+
+public class PancakeHouseMenu {
+
+	ArrayList menuItems;
+
+	public PancakeHouseMenu() {
+		menuItems = new ArrayList();
+
+		addItem("K&B¡¯s Pancake Breakfast",
+				"Pancakes with scrambled eggs, and toast", true, 2.99);
+		addItem("Regular Pancake Breakfast",
+				"Pancakes with fried eggs, sausage", false, 2.99);
+	}
+
+	public void addItem(String name, String descr, boolean vegetarian,
+			double price) {
+		MenuItem menuItem = new MenuItem(name, descr, vegetarian, price);
+		menuItems.add(menuItem);
+	}
+
+	public ArrayList getMenuItems() {
+		return menuItems;
+	}
+
+}
